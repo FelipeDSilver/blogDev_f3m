4864176c3ac72af8c675876ecc72c112ed8c8757
FelipeDSilver <felipedonsilva@hotmail.com>
2023-10-31 12:34:59 -0300

    aula3110 Estrutueas de CSS e navegação

---
+++ front-blogdev/src/pages/Register/Register.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        <h1>Compartilhe suas experiências com outros nomades</h1>
+        <form>
+            <label>
+                <span>Nome: </span>
+            
+            <input 
+            type="text"
+            name='displayName'
+            required
+            placeholder='Entre com seu nomade nome'></input>
+            </label>
+            <label>
+                <span>E-mail: </span>
+            <input 
+            type="email"
+            name='email'
+            required
+            placeholder='Entre com sua Senha'></input>
+            </label>
+            <label>
+                <span>Senha: </span>
+            <input 
+            type="password"
+            name='password'
+            required
+            placeholder='Entre com sua senha'></input>
+            </label>
+            <label>
+                <span>Confirmação: </span>
+            <input 
+            type="password"
+            name='confirmedPassword'
+            required
+            placeholder='Confirme sua senha'></input>
+            </label>
+            <button className='btn'>Cadastrar</button>
+        </form>
+    </div>
+  )
+}
+
+export default Register
